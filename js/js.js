2a9392125e21e2826eb86dd4d92d2ee781aaa976
Alien Q <celestialq7@gmail.com>
2026-03-23 22:26:38 +0530

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,41 @@
+const gallery = document.getElementById("gallery");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+
+fetch("https://trackbit.nielitkohima.in/fetch_images.php")
+.then(r=>r.json())
+.then(({data})=>{
+
+  data.sort((a,b)=>new Date(a.uploaded_at)-new Date(b.uploaded_at));
+
+  data.forEach(i=>{
+
+    const col=document.createElement("div");
+    col.className="col-6 col-md-4 col-lg-3 mb-3";
+
+    const card=document.createElement("div");
+    card.className="card";
+
+    const img=document.createElement("img");
+    img.src=i.file_path;
+
+    const overlay=document.createElement("div");
+    overlay.className="overlay";
+    overlay.innerHTML=`
+      <strong>${i.title}</strong>
+      <small>${new Date(i.uploaded_at).toLocaleDateString()}</small>
+    `;
+
+    img.onclick=()=>{
+      modalImg.src=i.file_path;
+      modal.classList.add("active");
+    };
+
+    card.append(img,overlay);
+    col.appendChild(card);
+    gallery.appendChild(col);
+  });
+
+});
+
+modal.onclick=()=>modal.classList.remove("active");
